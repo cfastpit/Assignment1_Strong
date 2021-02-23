46c4f05541b0e698ccf0ff46c92db3efb2cd46ca
0778430 <0778430@ACIS-TUTOR04-P.pittstate.edu>
2021-02-23 15:22:16 -0600

    somthing

---
+++ Assignment1/src/java/business/User.java
@@ -0,0 +1,51 @@
+package business;
+
+import database.UserDB;
+import java.io.Serializable;
+
+public class User implements Serializable {
+
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    public User() {
+        firstName = "";
+        lastName = "";
+        email = "";
+    }
+
+    public User(String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    public void add(){
+        UserDB.add(this);
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
---
+++ Assignment1/src/java/database/UserDB.java
@@ -0,0 +1,10 @@
+package database;
+
+import business.User;
+
+public class UserDB {
+
+    public static void add(User user) {
+        // TODO: Add code that adds the user to the database
+    }
+}
